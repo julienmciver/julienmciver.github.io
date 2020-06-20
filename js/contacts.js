a88f2db01e8b81f5ed52863f4fb5d50e635705c9
julienmciver <34250945+julienmciver@users.noreply.github.com>
2020-06-21 01:14:57 +0200

    let's see

---
+++ js/contacts.js
@@ -0,0 +1,83 @@
+// When the user clicks on div, open the popup
+function fadeIn_popup() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.add("show");
+    popup.classList.remove("hide");
+    
+  }
+
+function fadeOut_popup(){
+    var popup = document.getElementById("myPopup");
+    popup.classList.remove("show");
+    popup.classList.add("hide");
+    
+}
+
+function fadeIn_popup_2() {
+    var popup = document.getElementById("myPopup_2");
+    changePopup();
+    popup.classList.add("show");
+    popup.classList.remove("hide");
+    
+  }
+
+function fadeOut_popup_2(){
+    var popup = document.getElementById("myPopup_2");
+    popup.classList.remove("show");
+    popup.classList.add("hide");
+    
+}
+
+function fadeIn_popup_3() {
+    var popup = document.getElementById("myPopup_3");
+    changePopup();
+    popup.classList.add("show");
+    popup.classList.remove("hide");
+    
+  }
+
+function fadeOut_popup_3(){
+    var popup = document.getElementById("myPopup_3");
+    popup.classList.remove("show");
+    popup.classList.add("hide");
+    
+}
+
+
+//get a new date-time, figure out if it is a date-time. If certain sections of it are outside of certain boundaires. I'll do the logic piece by piece.
+function checkDate(){
+    var d = new Date();
+    
+    d2 = d.toString().split(" ");
+    console.log(d2);
+
+    if (d2[0] == "Sat" || d2[0] == "Sun"){
+        return false;
+    }
+    else{
+        d3 = d2.split(":");
+        if (d3[0] <= 16 && d3[0] >= 7){
+            if (d3[1] < 30){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+    }
+}
+
+function changePopup(){
+    if (checkDate()){
+        document.getElementById("myPopup_2").innerHTML = "We are Open";
+    }
+    else{
+        document.getElementById("myPopup_2").innerHTML = "We are Closed<br><i style = \"font-size: 2rem;\"class=\"fas m-2 fa-clock icon\"></i>";
+    }
+}
+
+checkDate();
+document.getElementById("map-link").addEventListener("mouseleave", fadeOut_popup);
+document.getElementById("email-div").addEventListener("mouseleave", fadeOut_popup_3);
+document.getElementById("phone-div").addEventListener("mouseleave", fadeOut_popup_2);
